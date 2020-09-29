e01476b35e3424d0e32885f6e81ba21489d5ae9e
Yaroslav <keyla.prezent@gmail.com>
2020-09-30 01:21:02 +0300

    Added DB abd refactored routes.

--- server.js
+++ server.js
@@ -1,72 +1,101 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+const config = require('./config/config');
+
 
 let app = express();
+let db;
+
+const uri = config.uri;
+//const uri = 'mongodb+srv://niyak:byt456UN@users-api.w3jjo.mongodb.net/usersAPI?retryWrites=true&w=majority';
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-let users = [
-  {
-    id: 1,
-    login: 'niyak4',
-    username: 'Ka4a'
-  },
-  {
-    id: 2,
-    login: 'konyaka083',
-    username: 'Scarecrow'
-  },
-  {
-    id: 3,
-    login: 'sinokoss',
-    username: 'vvlvchk'
-  }
-];
 
 app.get('/', (req, res) => {
   res.send("Welcome!");
 });
 
 app.get('/users', (req, res) => {
-  res.send(users);
-  console.log(users);
+  db.collection('users').find().toArray(( err, users) => {
+    if (err) {
+      console.log('Error:', err);
+      return res.sendStatus(500);
+    }
+    res.send(users);
+  });
 });
 
 app.post('/users', (req, res) => {
   let user = {
-    id: Date.now(),
     login: req.body.login,
     username: req.body.username
   }
-  users.push(user);
-  res.send(user);
+
+  db.collection('users').insert(user, (err, result) => {
+    if (err) {
+      console.log('Error:', err);
+      return res.sendStatus(500);
+    }
+    res.send(user);
+  });
   console.log(user);
 });
 
 app.get('/users/:id', (req, res) => {
-  let user = users.find((user) => {
-    return user.id === Number(req.params.id);
-  });
-  res.send(user);
-  console.log(user);
+  db.collection('users').findOne({ '_id': ObjectID(req.params.id) }, (err, user) => {
+    if (err) {
+      console.log('Error:', err);
+      return res.sendStatus(500);
+    }
+    res.send(user);
+  })
 });
 
 app.put('/users/:id', (req, res) => {
-  let user = users.find((user) => {
-    return user.id === Number(req.params.id);
-  });
-  user.username = req.body.username;
-  res.sendStatus(200);
+  db.collection('users').updateOne(
+    { '_id': ObjectID(req.params.id) },
+    [
+      { $set: { 'username': req.body.username, 'login': req.body.login } },
+    ],
+    (err) => {
+      if (err) {
+        console.log('Error:', err);
+        return res.sendStatus(500);
+      }
+      res.sendStatus(200);
+    }
+  )
 });
 
 app.delete('/users/:id', (req, res) => {
-  users = users.filter((user) => {
-    return user.id !== Number(req.params.id);
-  })
-  res.sendStatus(200);
+  db.collection('users').deleteOne(
+    { '_id': ObjectID(req.params.id) },
+    (err) => {
+      if (err) {
+        console.log('Error:', err);
+        return res.sendStatus(500);
+      }
+      res.sendStatus(200);
+    }
+  )
 });
 
-app.listen(3000, () => {
-  console.log("Server started.");
+
+client.connect(err => {
+  if (err) {
+    console.log(err);
+    return 0;
+  }
+  
+  db = client.db('usersAPI');
+
+  app.listen(3000, () => {
+    console.log('Server started.');
+  });
+
+  //client.close();
 });
